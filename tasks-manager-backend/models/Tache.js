1978aa9379b3896f83449a3134acde16a7b94474
Corentin75 <corentindetournay05@gmail.com>
2025-11-18 17:56:26 +0100

    "Tache" model

---
+++ tasks-manager-backend/models/Tache.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const tacheSchema = new mongoose.Schema({
+  titre: String,
+  description: String,
+  dateCreation: { type: Date, default: Date.now },
+  echeance: Date,
+  statut: String,
+  priorite: String,
+  auteur: {
+    nom: String,
+    prenom: String,
+    email: String
+  },
+  categorie: String,
+  etiquettes: [String],
+  sousTaches: [
+    {
+      titre: String,
+      statut: String,
+      echeance: Date
+    }
+  ],
+  commentaires: [
+    {
+      auteur: String,
+      date: { type: Date, default: Date.now },      
+      contenu: String
+    }
+  ],
+  historiqueModifications: [
+    {
+      champModifié: String,
+      ancienneValeur: String,
+      nouvelleValeur: String,
+      date: { type: Date, default: Date.now }
+    }
+  ]
+});
+
+module.exports = mongoose.model('Tache', tacheSchema);
