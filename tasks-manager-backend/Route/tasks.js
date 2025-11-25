b1f3b6a986fb71526a56c92f699b44e00d35ac8a
enzB29 <enzo.bordet43@gmail.com>
2025-11-25 10:50:12 +0100

    add file tasks.js

---
+++ tasks-manager-backend/Route/tasks.js
@@ -0,0 +1,10 @@
+//  GET /tasks : récupérer toutes les tâches.
+// GET /tasks/:id : récupérer une tâche par son identifiant.
+// POST /tasks : créer une nouvelle tâche.
+// PUT /tasks/:id : modifier une tâche existante.
+// DELETE /tasks/:id : supprimer une tâche.
+// Fonctionnalités complémentaires obligatoires :
+// Filtrer les tâches (statut, priorité, catégorie, étiquette, échéance).
+// Trier les résultats (par date, priorité, etc.).
+// Gérer les sous-tâches et commentaires.
+// (Optionnel) Historiser les modifications
